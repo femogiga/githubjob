3ee46062f0c08f4dfbf6a3f080beb604d182ca80
femogiga <femogiga@yahoo.com>
2023-08-14 19:35:23 +0100

    front component complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Card from './components/Card'
+import MainForm from './components/MainForm'
 import Search from './components/Search'
 
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className='app'>
       <Search/>
+      <MainForm/>
       <Card/>
     </div>
   )
--- src/components/MainForm.tsx
+++ src/components/MainForm.tsx
@@ -1,8 +1,40 @@
-const MainForm = ()=>{
-    return(
-        <div className="">
+import style from '../css/mainform.module.css'
+import PublicIcon from '@mui/icons-material/Public';
 
-        </div>
+const MainForm = ()=>{
+
+    return(
+        <form className={style.form}>
+            <fieldset className='flex gap-1'>
+                <input type ='checkbox' className={style.checkbox} id="fulltime" name="fullttime"/><label htmlFor="fulltime">Full time</label>
+            </fieldset>
+            <fieldset>
+                <p className='flow-05'>Location</p>
+                <div className={style.rel} style={{color:'grey'}}>
+                    <PublicIcon className={style.icon} />
+                    <input type='text' className={style.textfield} placeholder='City, state, zip code or country' />
+                </div>
+            </fieldset>
+            <fieldset className={style.radioCont}>
+                <div>
+                    <input type='radio' name = 'location' value ='London' id='london' className={`${style.radio}`}/>
+                    <label htmlFor="">  London</label>
+                </div>
+                <div>
+                <input type='radio' name = 'location' value ='London' id='amsterdam' className={style.radio}/>
+                    <label htmlFor="">  Amsterdam</label>
+                 </div>
+                <div>
+                    <input type='radio' name = 'location' value ='London' id='newyork' className={style.radio}/>
+                    <label htmlFor=""> New York</label>
+                </div>
+                <div>
+                    <input type='radio' name = 'location' value ='London' id='berlin'  className={style.radio} />
+                    <label htmlFor=""> Berlin</label>
+                </div>
+
+            </fieldset>
+        </form>
     )
 }
 
